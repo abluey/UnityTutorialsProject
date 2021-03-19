2019b50b04d70efd4458f80ffadca0345995e1a2
abluey <zoe1615@hotmail.com>
2021-03-19 20:41:33 +0000

    Workshop01

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button startGameButton;
+    [SerializeField] private string startSceneName;
+
+    void Start()
+    {
+        startGameButton.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(startSceneName, LoadSceneMode.Single);
+        asyncLoad.completed += OnStartGame;
+    }
+
+    private void OnStartGame(AsyncOperation loadOperation)
+    {
+        Debug.Log("Loaded start scene.");
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
